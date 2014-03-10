27db7f1590d59c3b166493401d16aa73dbab5c9b
Martin Harari Thuresson <martin.thure@gmail.com>
2014-03-10 07:44:14 +0100

    operators and types added

--- trafictype.py
+++ trafictype.py
@@ -4,13 +4,22 @@ import re
 #    reader = csv.reader(f)
 #    for row in reader:
 #        print row
+# https://api.trafiklab.se/samtrafiken/gtfs/sweden.zip?key=wItxkyLvncTsCYzXUwADmAIAvX4kWPES
 
 trips = {}
+ftrips = {}
 with open('sweden/trips.txt', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
 	if not row[0] == 'route_id':
-        	trips[row[2]] = re.sub('_|[0-9]', '', row[0])
+       		trips[row[2]] = re.sub('_|[0-9]', '', row[0])
+		ftrips[row[2]] = row
+route = {}
+with open('sweden/routes.txt', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+	if not row[0] == 'route_id':
+        	route[row[0]] = row
 
 stops = {}
 types = {}
@@ -23,6 +32,11 @@ with open('sweden/stop_times.txt', 'r') as f:
 				stops[row[3]] = {}
 				stops[row[3]]['id'] = row[3]
 			stops[row[3]][trips[row[0]]] = 1
+			thisroute = route[ftrips[row[0]][0]]
+			stops[row[3]]['op'+thisroute[1]] = 1
+			stops[row[3]]['type'+thisroute[5]] = 1
+			types['op'+thisroute[1]] = 'op'+thisroute[1]
+			types['type'+thisroute[5]] = 'type'+thisroute[5]
 			types[trips[row[0]]] = trips[row[0]]
 		except:
 			print 'Hoppar rubrikrader'
@@ -30,13 +44,13 @@ with open('sweden/stop_times.txt', 'r') as f:
 f = open('travelmodes.csv','w')
 
 f.write('ID')
-for typ in types:
+for typ in sorted(types.iterkeys()):
 	 f.write(',' + typ)
 f.write('\n')
 
 for row in stops:
 	f.write(row)
-	for typ in types:
+	for typ in sorted(types.iterkeys()):
 		if typ in stops[row]:
 			f.write(',1')
 		else:
