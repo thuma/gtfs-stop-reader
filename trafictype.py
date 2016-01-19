0f3f232bf5a30869d6899b8443de59538ef9a6c5
Martin Harari Thuresson <martin@46elks.com>
2016-01-19 12:45:03 +0100

    Added support for exteded route types

--- trafictype.py
+++ trafictype.py
@@ -3,53 +3,55 @@ import re
 
 trips = {}
 ftrips = {}
+
+# Load all trips to hash
 with open('../sweden/trips.txt', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
-	if not row[0] == 'route_id':
-       		trips[row[2]] = re.sub('_|[0-9]', '', row[0])
-		ftrips[row[2]] = row
+        if not row[0] == 'route_id':
+            trips[row[2]] = row
+
+# Load all routes to hach
 route = {}
 with open('../sweden/routes.txt', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
-	if not row[0] == 'route_id':
-        	route[row[0]] = row
+        if not row[0] == 'route_id':
+            route[row[0]] = row
 
+# Map all trimes -> trip -> route and add type to new data
 stops = {}
 types = {}
 with open('../sweden/stop_times.txt', 'r') as f:
     reader = csv.reader(f) 
     for row in reader:
-	if not row[0] == 'trip_id':
-		try:
-			if not row[3] in stops:
-				stops[row[3]] = {}
-				stops[row[3]]['id'] = row[3]
-			stops[row[3]][trips[row[0]]] = 1
-			thisroute = route[ftrips[row[0]][0]]
-			stops[row[3]]['op'+thisroute[1]] = 1
-			stops[row[3]]['type'+thisroute[5]] = 1
-			types['op'+thisroute[1]] = 'op'+thisroute[1]
-			types['type'+thisroute[5]] = 'type'+thisroute[5]
-			types[trips[row[0]]] = trips[row[0]]
-		except:
-			print 'Hoppar rubrikrader'
+        if not row[0] == 'trip_id':
+            if not row[3] in stops:
+                stops[row[3]] = {}
+                stops[row[3]]['id'] = row[3]
 
+            thisroute = route[trips[row[0]][0]]
+            stops[row[3]]['agency_'+thisroute[1]] = 1
+            stops[row[3]]['type_'+thisroute[4]] = 1
+            types['agency_'+thisroute[1]] = 'agency_'+thisroute[1]
+            types['type_'+thisroute[4]] = 'type_'+thisroute[4]
+
+# Make new file.
 f = open('travelmodes.csv','w')
 
+# Write to file:
 f.write('ID')
 for typ in sorted(types.iterkeys()):
-	 f.write(',' + typ)
+     f.write(',' + typ)
 f.write('\n')
 
 for row in stops:
-	f.write(row)
-	for typ in sorted(types.iterkeys()):
-		if typ in stops[row]:
-			f.write(',1')
-		else:
-			f.write(',')
-	f.write('\n')
+    f.write(row)
+    for typ in sorted(types.iterkeys()):
+        if typ in stops[row]:
+            f.write(',1')
+        else:
+            f.write(',')
+    f.write('\n')
 f.close()
 
